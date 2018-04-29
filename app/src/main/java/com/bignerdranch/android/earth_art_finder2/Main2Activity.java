509a3e8d171d7dae8678f1b6bd8467e2ad73e3c3
Bradone911 <baholland1@buffs.wtamu.edu>
2018-04-28 19:29:41 -0500

    added the camera button, works!!

--- app/src/main/java/com/bignerdranch/android/earth_art_finder2/Main2Activity.java
+++ app/src/main/java/com/bignerdranch/android/earth_art_finder2/Main2Activity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.earth_art_finder2;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
+
+    ImageView mImageView;
 
 
     @Override
@@ -44,5 +48,23 @@ public class Main2Activity extends AppCompatActivity {
 
         });
 
+        Button btnCamera = (Button) findViewById(R.id.btnCamera);
+        ImageView mImageView= (ImageView) findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int reqeustCode, int resultCode, Intent data)   {
+        super.onActivityResult(reqeustCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        mImageView.setImageBitmap(bitmap);
     }
 }
+
